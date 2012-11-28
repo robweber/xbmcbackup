18c85870ece81e865853704a5d46e43e0155f8d0
Rob Weber <robweberjr@gmail.com>
2012-11-28 08:54:23 -0600

    moved mode selection to gui

--- default.py
+++ default.py
@@ -1,7 +1,13 @@
+import xbmcgui
+import resources.lib.utils as utils
 from resources.lib.backup import XbmcBackup
 
-#run the profile backup
-backup = XbmcBackup()
+#figure out if this is a backup or a restore
+mode = xbmcgui.Dialog().select(utils.getString(30023),[utils.getString(30016),utils.getString(30017)])
 
-if(backup.isReady()):
-    backup.run()
+if(mode != -1):
+    #run the profile backup
+    backup = XbmcBackup()
+
+    if(backup.isReady()):
+        backup.run(mode)

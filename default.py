926382e28efbbba0a340d2bea3dd0fd85eff081e
Rob Weber <robweberjr@gmail.com>
2012-11-28 09:47:40 -0600

    updated title

--- default.py
+++ default.py
@@ -3,11 +3,12 @@ import resources.lib.utils as utils
 from resources.lib.backup import XbmcBackup
 
 #figure out if this is a backup or a restore
-mode = xbmcgui.Dialog().select(utils.getString(30023),[utils.getString(30016),utils.getString(30017)])
+mode = xbmcgui.Dialog().select(utils.getString(30010) + " - " + utils.getString(30023),[utils.getString(30016),utils.getString(30017)])
 
 if(mode != -1):
     #run the profile backup
     backup = XbmcBackup()
 
     if(backup.isReady()):
+        
         backup.run(mode)

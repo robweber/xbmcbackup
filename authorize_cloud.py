c46f684ea5567c56fb63486502a40da305b561d6
Rob Weber <rweber@ecec.com>
2017-01-31 09:07:49 -0600

    change this as settings are saved until close, call specific type with arg

--- authorize_cloud.py
+++ authorize_cloud.py
@@ -1,20 +1,34 @@
-import xbmc
+import sys
+import urlparse
 import xbmcgui
-import xbmcvfs
 import resources.lib.utils as utils
 from resources.lib.authorizers import DropboxAuthorizer,GoogleDriveAuthorizer
 
+def get_params():
+    param = {}
+    try:
+        for i in sys.argv:
+            args = i
+            if(args.startswith('?')):
+                args = args[1:]
+            param.update(dict(urlparse.parse_qsl(args)))
+    except:
+        pass
+    return param
+
+params = get_params()
+
 #drobpox
-if(utils.getSetting('remote_selection') == '2'):
+if(params['type'] == 'dropbox'):
     authorizer = DropboxAuthorizer()
 
     if(authorizer.authorize()):
-        xbmcgui.Dialog().ok("Backup",utils.getString(30027) + ' ' + utils.getString(30106))
+        xbmcgui.Dialog().ok(utils.getString(30010),utils.getString(30027) + ' ' + utils.getString(30106))
     else:
-        xbmcgui.Dialog().ok("Backup",utils.getString(30107) + ' ' + utils.getString(30027))
+        xbmcgui.Dialog().ok(utils.getString(30010),utils.getString(30107) + ' ' + utils.getString(30027))
 
 #google drive
-elif(utils.getSetting('remote_selection') == '3'):
+elif(params['type'] == 'google_drive'):
     authorizer = GoogleDriveAuthorizer()
 
     if(authorizer.authorize()):

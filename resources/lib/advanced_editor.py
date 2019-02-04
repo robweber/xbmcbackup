85306f9469b614dcaff0a109ec1a6c2c30578512
Rob Weber <rweber@ecec.com>
2019-02-04 11:57:36 -0600

    closes #132

--- resources/lib/advanced_editor.py
+++ resources/lib/advanced_editor.py
@@ -177,10 +177,13 @@ class AdvancedBackupEditor:
 
     def copySimpleConfig(self):
         #disclaimer in case the user hit this on accident
-        shouldContinue = self.dialog.yesno('Copy Config','This will copy the current Simple file selection to the Advanced Editor','This will erase any current Advanced Editor settings')
+        shouldContinue = self.dialog.yesno('Copy Config','This will copy the default Simple file selection to the Advanced Editor','This will erase any current Advanced Editor settings')
 
         if(shouldContinue):
-            
+            source = xbmc.translatePath(utils.addon_dir() + "/resources/data/default_files.json")
+            dest = xbmc.translatePath(utils.data_dir() + "/custom_paths.json")
+
+            xbmcvfs.copy(source,dest)
 
               
 
